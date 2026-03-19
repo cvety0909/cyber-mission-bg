0b569726fdb44b5a1874d1a6786ea2ee2fe874fe
Tsvetislava <cvety0909@gmail.com>
2026-03-19 22:13:02 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cyber-mission-bg/",
   server: {
     host: "::",
     port: 8080,
